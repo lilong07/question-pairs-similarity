14b315786edb4ed7ccd51d6dfb2362884c6caa25
Li Long <eric.longlee@foxmail.com>
2018-06-10 19:46:21 +0800

    delete bin

--- bin/__init__.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/6/18 22:09
-# @Author  : HouJP
-# @Email   : houjp1992@gmail.com
--- bin/feature_engineering/__init__.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/6/18 10:39
-# @Author  : HouJP
-# @Email   : houjp1992@gmail.com
